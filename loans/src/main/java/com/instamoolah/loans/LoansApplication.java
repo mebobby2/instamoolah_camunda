4d8c87ab4a26c080bb0d3898d12e68c944a93c54
Bobby Lei <bobl53@gmail.com>
2021-04-21 15:32:50 +1200

    Bootstrap a simple process definition

--- loans/src/main/java/com/instamoolah/loans/LoansApplication.java
+++ loans/src/main/java/com/instamoolah/loans/LoansApplication.java
@@ -1,13 +1,27 @@
 package com.instamoolah.loans;
 
+import org.springframework.context.event.EventListener;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.camunda.bpm.engine.RuntimeService;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class LoansApplication {
+
+	@Autowired
+  private RuntimeService runtimeService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoansApplication.class, args);
 	}
+
+	@EventListener
+  private void processPostDeploy(PostDeployEvent event) {
+    runtimeService.startProcessInstanceByKey("newLoan");
+  }
 
 }

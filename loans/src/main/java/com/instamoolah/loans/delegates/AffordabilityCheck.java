8eb238650783e9e090dd1196adb71d7c68355c52
Bobby Lei <bobl53@gmail.com>
2021-04-23 11:01:07 +1200

    Executing dmn

--- loans/src/main/java/com/instamoolah/loans/delegates/AffordabilityCheck.java
+++ loans/src/main/java/com/instamoolah/loans/delegates/AffordabilityCheck.java
@@ -6,6 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.engine.DecisionService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
 
 @Component("checkAffordability")
 public class AffordabilityCheck implements JavaDelegate {
@@ -16,6 +22,19 @@ public class AffordabilityCheck implements JavaDelegate {
 
   @Override
   public void execute(DelegateExecution execution) {
-    LOGGER.info("AffordabilityCheck delegate called");
+    LOGGER.info("AffordabilityCheck called");
+
+    ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+    DecisionService decisionService = engine.getDecisionService();
+
+    VariableMap variables = Variables.createVariables()
+      .putValue("emailVerified", false)
+      .putValue("collectionStatus", "HARDSHIP")
+      .putValue("riskScore", 100);
+
+    DmnDecisionTableResult dishDecisionResult = decisionService.evaluateDecisionTableByKey("affordability", variables);
+    Boolean affordabilityApproved = dishDecisionResult.getSingleEntry();
+
+    LOGGER.info("\n\nAffordability Approved: {0}\n\n", affordabilityApproved);
   }
 }

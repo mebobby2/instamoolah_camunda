4376434680ce1b94319e4fd31e00969732afa31c
Bobby Lei <bobl53@gmail.com>
2021-04-29 11:38:54 +1200

    Adding a funding module

---
+++ funding/src/main/java/com/instamoolah/funding/FundingApplication.java
@@ -0,0 +1,26 @@
+package com.instamoolah.loans;
+
+import org.springframework.context.event.EventListener;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.instamoolah") // So it can packages inside the reactive module as well, which is a library
+@EnableProcessApplication
+public class FundingApplication {
+	@Autowired
+  private RuntimeService runtimeService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundingApplication.class, args);
+	}
+
+	@EventListener
+  private void processPostDeploy(PostDeployEvent event) {
+		System.out.println("Camunda Process Engine deployed successfully");
+  }
+
+}

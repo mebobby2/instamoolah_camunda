366cb8f6edbe31e3c3611455951dfebd04e0d957
Bobby Lei <bobl53@gmail.com>
2021-04-29 14:52:58 +1200

    Use message payloads in reactive libary

--- funding/src/main/java/com/instamoolah/funding/messages/MessageListener.java
+++ funding/src/main/java/com/instamoolah/funding/messages/MessageListener.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.context.event.EventListener;
 import com.instamoolah.funding.channels.FundingChannel;
-import com.instamoolah.funding.messages.ReserveFundsCommandPayload;
+import com.instamoolah.reactive.messages.ReserveFundsCommandPayload;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import com.instamoolah.reactive.messages.Message;
 
@@ -15,10 +15,9 @@ public class MessageListener {
 
 	@StreamListener(FundingChannel.RESERVE_FUNDS)
   public void reserveFundsEvent(Message<ReserveFundsCommandPayload> message) throws JsonParseException, JsonMappingException, IOException {
-    System.out.println("=======================================================");
     ReserveFundsCommandPayload command = message.getData();
 
-    System.out.println("Reserve Funds command received. " + command);
+    System.out.println("Reserve Funds command received. " + command.getAmount());
 
     // and kick of a new flow instance
     // camunda.getRuntimeService().createMessageCorrelation(message.getType())

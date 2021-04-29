647759de959ed33ae1a5f2c3de1297fe2bcdd0dd
Bobby Lei <bobl53@gmail.com>
2021-04-30 11:35:12 +1200

    Main bpmn process listening to events and finishing the flow

---
+++ loans/src/main/java/com/instamoolah/loans/messages/MessageListener.java
@@ -0,0 +1,54 @@
+package com.instamoolah.laons.messages;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.instamoolah.loans.channels.LoanChannel;
+import com.instamoolah.reactive.messages.GenerateContractCommandPayload;
+import com.instamoolah.reactive.messages.Message;
+import com.instamoolah.reactive.messages.ReserveFundsCommandPayload;
+import com.instamoolah.reactive.pipes.Channels;
+import java.io.IOException;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.event.EventListener;
+
+@EnableBinding(LoanChannel.class)
+public class MessageListener {
+
+  @Autowired
+  private RuntimeService runtimeService;
+
+  @StreamListener(Channels.FUNDS_RESERVED)
+  public void fundsGenerated(Message<ReserveFundsCommandPayload> message)
+    throws JsonParseException, JsonMappingException, IOException {
+    ReserveFundsCommandPayload command = message.getData();
+
+    System.out.println(
+      "Funds Reserved event received. " + command.getAmount()
+    );
+
+    runtimeService
+      .createMessageCorrelation(message.getType())
+      .processInstanceBusinessKey(message.getTraceid())
+      .correlateWithResult();
+  }
+
+  @StreamListener(Channels.CONTRACT_GENERATED)
+  public void contractGenerated(
+    Message<GenerateContractCommandPayload> message
+  )
+    throws JsonParseException, JsonMappingException, IOException {
+    GenerateContractCommandPayload command = message.getData();
+
+    System.out.println(
+      "Contract Generated event received. " + command.getAmount()
+    );
+
+    runtimeService
+      .createMessageCorrelation(message.getType())
+      .processInstanceBusinessKey(message.getTraceid())
+      .correlateWithResult();
+  }
+}
